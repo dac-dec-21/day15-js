c8ddd5a207dbd8317d02286a8888a75d3959ab56
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2021-12-30 14:06:08 +0530

    15

--- 16.js
+++ 16.js
@@ -1,10 +1,10 @@
 let list = [
   { dt: 1640845800, temp: 21 },
   { dt: 1640932200, temp: 22 },
-  { dt: 1641018600, temp: 22 },
+  { dt: 1641018600, temp: 24 },
 ];
 
 for (let item of list) {
   let date = new Date(item.dt * 1000);
-  console.log(date.getDate(), item.temp);
+  console.log(`Date ${date.getDate()} Temprate is ${item.temp}`);
 }
